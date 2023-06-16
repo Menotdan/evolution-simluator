6bdf40c21408926aa1edef2d82b5789a674ed7e0
Menotdan <32620310+Menotdan@users.noreply.github.com>
2023-06-16 00:17:38 -0400

    Move math functions to math_utils

--- math_utils.py
+++ math_utils.py
@@ -15,3 +15,15 @@ def get_new_value(old: float, scale: float):
 
 def get_distance(object1, object2):
     return math.sqrt(math.pow(object1.x - object2.x, 2) + math.pow(object1.y - object2.y, 2))
+
+def get_move_towards_vector(self, object):
+        move_direction = np.asarray([object.x - self.x, object.y - self.y])
+        move_direction = normalize_vector(move_direction)
+
+        return move_direction * self.speed
+
+def get_move_away_vector(self, object):
+        move_direction = np.asarray([self.x - object.x, self.y - object.y])
+        move_direction = normalize_vector(move_direction)
+
+        return move_direction * self.speed
